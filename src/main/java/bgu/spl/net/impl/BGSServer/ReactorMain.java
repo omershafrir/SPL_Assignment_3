4edd36adc3336c99874514f98cef0cc5143ae2b7
omershafrir <omershafrirus@gmail.com>
2022-01-07 22:30:12 +0200

    dff

--- src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
@@ -1,4 +1,20 @@
 package bgu.spl.net.impl.BGSServer;
 
+import bgu.spl.net.api.MessageEncoderDecoderImpl;
+import bgu.spl.net.api.bidi.BidiMessagingProtocolImpl;
+import bgu.spl.net.srv.BaseServer;
+import bgu.spl.net.srv.Messages.Message;
+import bgu.spl.net.srv.Reactor;
+import bgu.spl.net.srv.Server;
+
 public class ReactorMain {
+    public static void main(String []args) {
+
+        int port = 217;
+        Server<Message> server = new Reactor<>(3, port,
+                () -> new BidiMessagingProtocolImpl(),
+                () -> new MessageEncoderDecoderImpl());
+
+        server.serve();
+    }
 }
